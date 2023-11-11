0ea07aa48019a884c553075bee27f23fcc0c200d
sara-417 <saraphant4@gmail.com>
2023-11-11 13:37:04 -0600

    100 crimes displayed with delay (no database)

--- app/src/main/java/com/slayton/msu/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/slayton/msu/criminalintent/CrimeListFragment.kt
@@ -8,8 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.slayton.msu.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.launch
 
 class CrimeListFragment : Fragment() {
 
@@ -21,11 +25,6 @@ class CrimeListFragment : Fragment() {
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,12 +33,20 @@ class CrimeListFragment : Fragment() {
         _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        val crimes = crimeListViewModel.crimes
-        val adapter = CrimeListAdapter(crimes)
-        binding.crimeRecyclerView.adapter = adapter
-
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val crimes = crimeListViewModel.loadCrimes()
+                binding.crimeRecyclerView.adapter =
+                    CrimeListAdapter(crimes)
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
