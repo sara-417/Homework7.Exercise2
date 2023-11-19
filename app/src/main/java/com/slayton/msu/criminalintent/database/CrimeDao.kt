0781c352ef3dead9c7e278b07d413c83a97421a1
sara-417 <saraphant4@gmail.com>
2023-11-19 14:32:01 -0600

    Chapter 13 finished

--- app/src/main/java/com/slayton/msu/criminalintent/database/CrimeDao.kt
+++ app/src/main/java/com/slayton/msu/criminalintent/database/CrimeDao.kt
@@ -3,6 +3,7 @@ package com.slayton.msu.criminalintent.database
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Update
 import com.slayton.msu.criminalintent.Crime
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
@@ -14,4 +15,7 @@ interface CrimeDao {
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
     suspend fun getCrime(id: UUID): Crime
+
+    @Update
+    suspend fun updateCrime(crime: Crime)
 }
